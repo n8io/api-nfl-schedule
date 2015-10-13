36dc4f19afe0e69ce5318f7d54c1b27e7a661ea2
Nate Clark <nathan.clark@ignitionone.com>
2015-10-12 22:07:00 -0400

    Split out integration tests

--- gulp/tasks/test-integration.js
+++ gulp/tasks/test-integration.js
@@ -1,10 +1,10 @@
 var path = require('path');
 
 module.exports = function(gulp, plugins, cfg) {
-  gulp.task('test', test);
+  gulp.task('test-integration', testIntegration);
 
-  function test(done) {
-    gulp.src(cfg.test.src)
+  function testIntegration(done) {
+    gulp.src(cfg.test.integration.src)
       .pipe(plugins.mocha())
       .on('end', done)
       ;
